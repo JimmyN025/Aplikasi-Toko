f0b772dbfe026fdeff4bb0ca3945ddb0fa66e924
JimmyN025 <76139887+JimmyN025@users.noreply.github.com>
2023-07-07 12:32:32 +0700

    From Jim

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
 <div class="row d-flex justify-content-center">
 <img src="img/tartaglias.png" alt="">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, cupiditate laboriosam nihil molestias, odio perferendis unde doloremque exercitationem quisquam, earum sed magnam officiis. Neque distinctio sapiente nisi maxime officia odio quam eligendi voluptate laboriosam ex consequatur labore deserunt, doloremque nesciunt animi omnis eius possimus totam modi dolorum ratione corrupti dolorem similique repellat! Voluptates distinctio nemo vel tempore quas praesentium, quis autem non animi, delectus alias hic numquam. Explicabo, culpa quam rem cupiditate totam ab veritatis facilis, earum fuga officiis suscipit nesciunt hic recusandae nisi ipsa, error vero eius labore? Suscipit fugiat ea dolor corporis animi obcaecati earum, delectus laborum iusto deserunt molestias unde repellendus, saepe quaerat velit, accusamus voluptas ipsa. Debitis quas esse veritatis non reiciendis? Autem, dicta, atque repudiandae eaque consequatur rerum reiciendis eligendi laudantium error quaerat delectus debitis saepe ratione veniam facere vero iure perferendis ex facilis porro deserunt suscipit numquam. Consequatur nemo possimus, dolore libero voluptate recusandae.</p>
 </div>
 </div>
 
